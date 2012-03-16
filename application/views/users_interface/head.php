577d6bc6442e757c546c392eab343a452c00ad94
Andrew G <andrew.samoilov@gmail.com>
2012-03-16 23:41:23 +0400

    remont page and css

--- application/views/users_interface/head.php
+++ application/views/users_interface/head.php
@@ -4,10 +4,10 @@
 	<title><?=$title;?></title>
 	<meta name="description" content="<?=$description;?>">
 	<meta name="viewport" content="width=device-width">
+	<link rel='stylesheet' href="http://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700,700italic&subset=latin,cyrillic">
 	<link rel="stylesheet" href="<?=$baseurl;?>css/boilerplate.css">
 	<link rel="stylesheet" href="<?=$baseurl;?>css/bootstrap.min.css">
 	<link rel="stylesheet" href="<?=$baseurl;?>css/960.css">
-	<link rel='stylesheet' href="http://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700,700italic&subset=latin,cyrillic">
 	<link rel="stylesheet" href="<?=$baseurl;?>css/style.css">
 	<link rel="shortcut icon" href="<?=$baseurl;?>img/favicon.png">
 	<link rel="apple-touch-icon" href="<?=$baseurl;?>img/apple-touch-icon.png">
