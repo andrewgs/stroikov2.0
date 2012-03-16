6819f1681161c73f603bfbcab95ad30ee75b229e
Andrew G <andrew.samoilov@gmail.com>
2012-03-17 01:59:02 +0400

    design interierov

--- application/views/users_interface/design-interierov.php
+++ application/views/users_interface/design-interierov.php
@@ -12,25 +12,27 @@
 		<section class="proposals">
 			<div class="grid_16 carousel list">
 			<?php if(count($interior)>0):?>
-				<h2><?=$interior[0]['rooms'].'-к квартира'.'<br/>'.$interior[0]['address'];?> <span class="details">Площадь <?=$interior[0]['area'];?>м<sup>2</sup></span></h2>
+				<h2><?=$interior[0]['title'].', '.$interior[0]['rooms'].'-комнаты'.'<br/>'.$interior[0]['address'];?> <span class="details">Площадь <?=$interior[0]['area'];?>м<sup>2</sup></span></h2>
 				<div class="grid_1">
-					<div class="slider-arrow left">Пред.</div>
+					<a href="#" id="prev" class="slider-arrow left">Пред.</a>
 				</div>
-				<div class="slider">
-					<div class="grid_14 alpha omega">
-						<div class="design-sample">
-						<?php for($i=0;$i<count($interior[0]['images']);$i++):?>
-							<img src="<?=$baseurl.$this->uri->uri_string();?>/viewimage/<?=$interior[0]['images'][$i]['id'];?>" alt=""/>
-						<?php endfor;?>
-							<?=anchor('design-interierov/'.$objects[0]['translit'].'/'.$interior[0]['translit'],$interior[0]['title']);?>
-							<p>
-								<?=$interior[0]['note'];?>
-							</p>
+				<div class="grid_14 alpha omega">
+					<div class="slider">
+						<div id="samples">
+							<?php for($i=0;$i<count($interior[0]['images']);$i++):?>
+							<div class="design-sample">
+								<img src="<?=$baseurl.$this->uri->uri_string();?>/viewimage/<?=$interior[0]['images'][$i]['id'];?>" alt=""/>
+							</div>
+							<?php endfor;?>
 						</div>
+						<?=anchor('design-interierov/'.$objects[0]['translit'].'/'.$interior[0]['translit'],$interior[0]['title']);?>
+						<p>
+							<?=$interior[0]['note'];?>
+						</p>
 					</div>
 				</div>
 				<div class="grid_1">
-					<div class="slider-arrow right">След.</div>
+					<a href="#" id="next" class="slider-arrow right">След.</a>
 				</div>
 			<?php else:?>
 				<?php if(isset($objects[0])):?>
@@ -52,9 +54,9 @@
 					<h3>Смотреть интерьеры</h3>
 					<ul>
 					<?php for($i=0;$i<count($objects);$i++):?>
-						<li><h4><?=$objects[$i]['title'];?></h4></li>
+						<li><?=$objects[$i]['title'];?></li>
 						<?php for($j=0;$j<count($objects[$i]['interiors']);$j++):?>
-							<li><?=anchor('design-interierov/'.$objects[$i]['translit'].'/'.$objects[$i]['interiors'][$j]['translit'],$objects[$i]['interiors'][$j]['title']);?></li>
+							<li><?=anchor('design-interierov/'.$objects[$i]['translit'].'/'.$objects[$i]['interiors'][$j]['translit'],$objects[$i]['interiors'][$j]['rooms'].'-к квартира');?> <?= $objects[$i]['interiors'][$j]['address']; ?></li>
 						<?php endfor;?>
 					<?php endfor;?>
 					</ul>
@@ -71,6 +73,7 @@
 	</div>
 	<?=$this->load->view('users_interface/scripts');?>
 	<?=$this->load->view('users_interface/google');?>
+	
 	<script type="text/javascript">
 		$(document).ready(function(){
 			$("#send").click(function(event){
@@ -87,6 +90,15 @@
 				if(err){event.preventDefault();}
 			});
 			$("#addInterior").on("hidden",function(){$(".control-group").removeClass('error');$(".help-inline").hide();});
+			
+			$('div#samples').cycle({
+				fx:     'scrollHorz',
+				speed:  '2000',					
+				easing: 'easeInOutExpo',
+				timeout:  7000,
+				prev:    '#prev',
+				next:    '#next'
+			});  	
 		});
 	</script>
 </body>
--- application/views/users_interface/scripts.php
+++ application/views/users_interface/scripts.php
@@ -1,6 +1,9 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="<?=$baseurl;?>js/libs/jquery-1.7.1.min.js"><\/script>')</script>
 <script src="<?=$baseurl;?>js/libs/bootstrap.min.js"></script>
+<script src="<?=$baseurl;?>js/jquery.easing.js"></script>
+<script src="<?=$baseurl;?>js/placeholder.jquery.js"></script>
+<script src="<?=$baseurl;?>js/jquery.cycle.js"></script>
 <script src="<?=$baseurl;?>js/plugins.js"></script>
 <script src="<?=$baseurl;?>js/script.js"></script>
 <script type="text/javascript">
